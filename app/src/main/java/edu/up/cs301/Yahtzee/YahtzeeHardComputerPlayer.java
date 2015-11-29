08b3405a94656e515361520fc829d8d5a04f8685
Michael Larios-Grave <lariosgr18@up.edu>
2015-11-29 13:14:16 -0800

    Created Hard AI

---
+++ app/src/main/java/edu/up/cs301/Yahtzee/YahtzeeHardComputerPlayer.java
@@ -0,0 +1,34 @@
+package edu.up.cs301.Yahtzee;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.infoMsg.GameInfo;
+
+/**
+ * Created by Michael on 11/29/2015.
+ */
+public class YahtzeeHardComputerPlayer extends GameComputerPlayer {
+
+    /**
+     * constructor
+     *
+     * @param name the player's name (e.g., "John")
+     */
+    public YahtzeeHardComputerPlayer(String name) {
+        super(name);
+    }
+
+    /**
+     * callback method--game's state has changed
+     *
+     * @param info
+     * 		the information (presumably containing the game's state)
+     */
+
+
+    @Override
+
+
+    protected void receiveInfo(GameInfo info) {
+
+    }
+}
